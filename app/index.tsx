83765d7a9b7a604501d5b3d8c01cbea6563a9721
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-01-01 21:31:06 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+       
+      }}
+    >
+      <Text>welcome bhushan youtube channle </Text>
+    </View>
+  );
+}
